db64c734730d06ff35e72f7e1f59ae126ee6370f
Kristen Foster-Marks <Kristenfoster-marks@Kristens-Macbrook-Pro.local>
2016-05-29 20:33:48 -0700

    added spinner for video progress

--- scripts/script.js
+++ scripts/script.js
@@ -7,4 +7,30 @@ var usersNativeLangFull = localStorage.nativeLanguageFull;
 console.log(usersNativeLang + " : " + usersNativeLangFull);
 
 var userNativeLangChoice = document.getElementById('users-native-lang-choice');
-userNativeLangChoice.innerHTML = "Lang: " + usersNativeLangFull;
+
+var userNativeLangChoiceTranslated;
+var urlString = "https://translate.yandex.net/api/v1.5/tr.json/translate?key=trnsl.1.1.20160520T131217Z.ce9eb7a558c632c6.b545c9193722b47e3faf7a417fe30acef59ab341&text=" + usersNativeLangFull + "&lang=en-" + localStorage.nativeLanguage;
+
+ajax('GET', urlString, function(err, data) {
+  if(!err) {
+    userNativeLangChoiceTranslated = data.text[0];
+    userNativeLangChoice.innerHTML = userNativeLangChoiceTranslated;
+  }
+});
+
+
+//AJAX REQUEST
+function ajax(method, url, handler) {
+  var req = new XMLHttpRequest();
+  req.onreadystatechange = function() {
+    if (req.readyState === 4) {
+      if(req.status === 200) {
+        handler(null, JSON.parse(req.responseText));
+      } else {
+        handler(req.status, null);
+      }
+    }
+  };
+  req.open(method, url);
+  req.send();
+}
